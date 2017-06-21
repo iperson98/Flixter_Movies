4d5bc1c00867b03790f1ab2b6f185e663a82b608
Alex Madera <amade002@ucr.edu>
2017-06-21 16:23:11 -0700

    Initial commit, started Video 3

---
+++ app/src/main/java/com/example/amade002/flicks/models/Movie.java
@@ -0,0 +1,17 @@
+package com.example.amade002.flicks.models;
+
+/**
+ * Created by amade002 on 6/21/17.
+ */
+
+public class Movie {
+
+    //values from API
+    private String title;
+    private String overview;
+    private String posterPath; // only the path
+
+    // intialize from the JSON data
+    public 
+
+}
