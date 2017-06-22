490e32cbbf96e7b83250fb72c1609b0ffb4f0207
Alex Madera <amade002@ucr.edu>
2017-06-22 14:25:02 -0700

    Applied Image with Glide and Alternate Orientation

---
+++ app/src/main/java/com/example/amade002/flicks/MovieAdapter.java
@@ -0,0 +1,8 @@
+package com.example.amade002.flicks;
+
+/**
+ * Created by amade002 on 6/21/17.
+ */
+
+public class MovieAdapter {
+}
---
+++ app/src/main/java/com/example/amade002/flicks/models/Config.java
@@ -0,0 +1,8 @@
+package com.example.amade002.flicks.models;
+
+/**
+ * Created by amade002 on 6/21/17.
+ */
+
+public class Config {
+}
